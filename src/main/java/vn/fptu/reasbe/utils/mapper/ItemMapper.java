e986ee1ef3bdb4f4d3e26969a71a066e475a003b
henrynguyen1203@gmail.com <henrynguyen1203@gmail.com>
2025-04-07 13:40:34 +0700

    Fix PayOS webhook url mapping

--- src/main/java/vn/fptu/reasbe/utils/mapper/ItemMapper.java
+++ src/main/java/vn/fptu/reasbe/utils/mapper/ItemMapper.java
@@ -44,8 +44,10 @@ public interface ItemMapper {
     @Mapping(target = "expiredTime", ignore = true)
     Item toEntity(UploadItemRequest dto);
 
+    @Mapping(target = "typeItem", source = "category.typeItem")
     ItemResponse toItemResponse(Item item);
 
+    @Mapping(target = "typeItem", source = "item.category.typeItem")
     @Mapping(target = "favorite", expression = "java(checkIfFavorite(item.getId(), favIds))")
     ItemResponse toItemResponseWithFavorite(Item item, List<Integer> favIds);
 
