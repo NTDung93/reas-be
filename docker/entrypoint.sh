7f1679ec18b48709840efdcc73ad764e47904e1a
Nguyễn Đức Sơn <113695548+Son-2003@users.noreply.github.com>
2025-04-09 16:24:52 +0700

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -14,7 +14,7 @@ echo "Waiting for PostgreSQL to be ready at $DB_HOST:$DB_PORT..."
 export PGPASSWORD=$DB_PASS
 
 echo "Checking available items in database..."
-AVAILABLE_ITEMS=$(psql -h $DB_HOST -U $DB_USER -d $DB_NAME -t -c "SELECT COUNT(*) FROM item WHERE status = 'AVAILABLE';" | xargs)
+AVAILABLE_ITEMS=$(psql -h $DB_HOST -U $DB_USER -d $DB_NAME -t -c "SELECT COUNT(*) FROM "ITEM" WHERE "STATUS_ITEM" = 'AVAI';" | xargs)
 
 if [ "$AVAILABLE_ITEMS" -eq "0" ]; then
   echo "No available items found. Importing initial data..."
